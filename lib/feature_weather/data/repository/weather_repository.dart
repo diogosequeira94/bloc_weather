c89d7c607adb3d9154c43a3b9ad05c620c112ddf
DiogoSequeira <Diogo.Sequeira@ctw.bmwgroup.com>
2020-10-10 16:08:30 +0100

    added weather_api_client for encapsulation

--- lib/feature_weather/data/repository/weather_repository.dart
+++ lib/feature_weather/data/repository/weather_repository.dart
@@ -1,40 +1,21 @@
-import 'dart:convert';
 import 'package:flutter_weather_bloclib/feature_weather/data/models/weather_model.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_weather_bloclib/feature_weather/data/network/weather_api_client.dart';
 import 'package:meta/meta.dart';
 
 abstract class WeatherRepository {
-  Future<int> getLocationId(String city);
-  Future<WeatherModel> fetchWeather(int locationId);
+  Future<WeatherModel> fetchWeather(String city);
 }
 
 class WeatherRepositoryImpl implements WeatherRepository {
-  static const baseUrl = 'https://www.metaweather.com';
-  final http.Client httpClient;
+  final WeatherApiClient weatherApiClient;
 
   WeatherRepositoryImpl({
-    @required this.httpClient
-  }) : assert(httpClient != null);
+    @required this.weatherApiClient
+  }) : assert(weatherApiClient != null);
 
   @override
-  Future<int> getLocationId(String city) async {
-    final locationUrl = '$baseUrl/api/location/search/?query=$city';
-    final locationResponse = await httpClient.get(locationUrl);
-    if(locationResponse.statusCode != 200){
-      throw Exception('error getting locationId for city');
-    }
-    final locationJson = jsonDecode(locationResponse.body) as List;
-    return(locationJson.first['woeid']);
-  }
-
-  @override
-  Future<WeatherModel> fetchWeather(int locationId) async {
-    final weatherUrl = '$baseUrl/api/location/$locationId';
-    final weatherResponse = await httpClient.get(weatherUrl);
-    if(weatherResponse.statusCode != 200) {
-      throw Exception('error getting the weather for location');
-    }
-    final weatherJson = jsonDecode(weatherResponse.body);
-    return WeatherModel.fromJson(weatherJson);
+  Future<WeatherModel> fetchWeather(String city) async {
+    final int locationId = await weatherApiClient.getLocationId(city);
+    return weatherApiClient.fetchWeather(locationId);
   }
 }
